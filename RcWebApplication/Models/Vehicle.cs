28a42f75e570a1274e18514866e4b4c155af4dfc
arcum-omni <travis.l.eiler@gmail.com>
2020-01-14 14:15:09 -0800

    Created Vehicle Class

---
+++ RcWebApplication/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RcWebApplication.Models
+{
+    public class Vehicle
+    {
+        public int VehicleID { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int BatterySize { get; set; }
+
+        public float MotorSize { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime BuildDate { get; set; }
+    }
+}
