38c96c17baf342cfa097aa96603063d4aca63f38
arcum-omni <travis.l.eiler@gmail.com>
2020-01-14 13:49:48 -0800

    Add project files.

---
+++ RcWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RcWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
